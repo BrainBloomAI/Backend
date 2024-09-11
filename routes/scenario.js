06dc0a351304f31d215e9871c8bf991c9699d51f
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-09-11 16:24:46 +0900

    added scenario delete

--- routes/scenario.js
+++ routes/scenario.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Scenario } = require('../models');
+const { Scenario, User } = require('../models');
 const { Logger, FileManager, Universal, Extensions } = require('../services');
 const { authoriseStaff } = require('../middleware/auth');
 const { storeImage } = require('../middleware/storeImage');
@@ -24,6 +24,14 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/new', authoriseStaff, async (req, res) => {
+    var staffUser;
+    try {
+        staffUser = await User.findByPk(req.userID, { attributes: ['userID', 'username'] });
+    } catch (err) {
+        Logger.log(`SCENARIO NEW ERROR: Failed to find user; error: ${err}`);
+        return res.status(500).send('ERROR: Failed to process request.');
+    }
+
     storeImage(req, res, async (err) => {
         if (err instanceof multer.MulterError) {
             Logger.log(`SCENARIO NEW ERROR: Image upload error; error: ${err}.`);
@@ -78,6 +86,8 @@ router.post('/new', authoriseStaff, async (req, res) => {
             if (!newScenario) {
                 return res.status(500).send('ERROR: Failed to create new scenario.');
             }
+
+            Logger.log(`SCENARIO NEW: New scenario '${newScenario.name}' created by '${staffUser.username}', scenario ID: ${newScenario.scenarioID}.`);
         } catch (err) {
             Logger.log(`SCENARIO NEW ERROR: Failed to create new scenario; error: ${err}`);
             return res.status(500).send('ERROR: Failed to create new scenario.');
@@ -92,6 +102,14 @@ router.post('/new', authoriseStaff, async (req, res) => {
 })
 
 router.post('/update', authoriseStaff, async (req, res) => {
+    var staffUser;
+    try {
+        staffUser = await User.findByPk(req.userID, { attributes: ['userID', 'username'] });
+    } catch (err) {
+        Logger.log(`SCENARIO NEW ERROR: Failed to find user; error: ${err}`);
+        return res.status(500).send('ERROR: Failed to process request.');
+    }
+
     storeImage(req, res, async (err) => {
         if (req.file) {
             if (err instanceof multer.MulterError) {
@@ -153,13 +171,7 @@ router.post('/update', authoriseStaff, async (req, res) => {
         if (modelRole) updateObject.modelRole = modelRole;
         if (userRole) updateObject.userRole = userRole;
         if (backgroundImageName) updateObject.backgroundImage = backgroundImageName;
-        try {
-            targetScenario.set(updateObject);
-            await targetScenario.save();
-        } catch (err) {
-            Logger.log(`SCENARIO UPDATE ERROR: Failed to update scenario; error: ${err}`);
-            return res.status(500).send('ERROR: Failed to update scenario.');
-        }
+
         if (Object.keys(updateObject).length === 0) {
             return res.send({
                 message: `SUCCESS: Nothing to update.`,
@@ -167,11 +179,73 @@ router.post('/update', authoriseStaff, async (req, res) => {
             })
         }
 
+        try {
+            targetScenario.set(updateObject);
+            await targetScenario.save();
+
+            Logger.log(`SCENARIO UPDATE: Scenario '${targetScenario.name}' updated by '${staffUser.username}', scenario ID: ${targetScenario.scenarioID}.`);
+        } catch (err) {
+            Logger.log(`SCENARIO UPDATE ERROR: Failed to update scenario; error: ${err}`);
+            return res.status(500).send('ERROR: Failed to update scenario.');
+        }
+
         return res.send({
             message: `SUCCESS: Scenario updated successfully.`,
             newScenario: Extensions.sanitiseData(targetScenario.toJSON(), [], ["createdAt", "updatedAt"])
         })
     })
+})
+
+router.delete('/delete', authoriseStaff, async (req, res) => {
+    var staffUser;
+    try {
+        staffUser = await User.findByPk(req.userID, { attributes: ['userID', 'username'] });
+    } catch (err) {
+        Logger.log(`SCENARIO NEW ERROR: Failed to find user; error: ${err}`);
+        return res.status(500).send('ERROR: Failed to process request.');
+    }
+
+    const { scenarioID, scenarioName } = req.body;
+    if (!scenarioID && !scenarioName) {
+        return res.status(400).send('ERROR: One or more required payloads not provided.')
+    }
+
+    var targetScenario;
+    try {
+        if (scenarioID) {
+            targetScenario = await Scenario.findByPk(scenarioID);
+        } else {
+            targetScenario = await Scenario.findOne({ where: { name: scenarioName } });
+        }
+
+        if (!targetScenario) {
+            return res.status(404).send('ERROR: Scenario not found.');
+        }
+    } catch (err) {
+        Logger.log(`SCENARIO DELETE ERROR: Failed to find scenario; error: ${err}`);
+        return res.status(500).send('ERROR: Failed to process request.');
+    }
+
+    // Delete image
+    try {
+        const fileDelete = await FileManager.deleteFile(targetScenario.backgroundImage);
+        if (fileDelete !== true) {
+            Logger.log(`SCENARIO DELETE ERROR: Failed to delete image; error: ${fileDelete}`);
+        }
+    } catch (err) {
+        Logger.log(`SCENARIO DELETE ERROR: Failed to delete image; error: ${err}`);
+    }
+
+    // Delete scenario
+    try {
+        await targetScenario.destroy();
+        Logger.log(`SCENARIO DELETE: Scenario '${targetScenario.name}' deleted by '${staffUser.username}', scenario ID: ${targetScenario.scenarioID}.`);
+    } catch (err) {
+        Logger.log(`SCENARIO DELETE ERROR: Failed to delete scenario; error: ${err}`);
+        return res.status(500).send('ERROR: Failed to delete scenario.');
+    }
+
+    return res.send('SUCCESS: Scenario deleted successfully.');
 })
 
 module.exports = { router, at: '/scenario' };
